b73a87e862c9fc0c2f2cf1b00311e929a4cb6d14
Xenon22pc <59983834+Xenon22pc@users.noreply.github.com>
2020-04-01 08:13:32 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,31 @@
+/* USER CODE BEGIN Includes */
+#include "display.h"
+#include "pacman.h"
+/* USER CODE END Includes */
+
+
+/* USER CODE BEGIN 0 */
+uint8_t btnRead()
+{
+	if(!HAL_GPIO_ReadPin(BTN1_GPIO_Port, BTN1_Pin)) JOY_DIR = UP;
+	if(!HAL_GPIO_ReadPin(BTN2_GPIO_Port, BTN2_Pin)) JOY_DIR = RIGHT;
+	if(!HAL_GPIO_ReadPin(BTN3_GPIO_Port, BTN3_Pin)) JOY_DIR = LEFT;
+	if(!HAL_GPIO_ReadPin(BTN4_GPIO_Port, BTN4_Pin)) JOY_DIR = DOWN;
+}
+/* USER CODE END 0 */
+
+
+/* USER CODE BEGIN 3 */
+	  btnRead();
+	  pacman_run(JOY_DIR);
+
+	  sprintf(str, "%u",Pacman.x);
+	  LCD_Puts(320-8*3, 0, str, clWhite, clBlack, &ASC8X16);
+	  sprintf(str, "%u",Pacman.y);
+	  LCD_Puts(320-8*3, 16, str, clWhite, clBlack, &ASC8X16);
+
+	  sprintf(str, "%u",point);
+	  LCD_Puts(320-8*3, 100, str, clWhite, clBlack, &ASC8X16);
+	  HAL_Delay(200);
+  }
+  /* USER CODE END 3 */

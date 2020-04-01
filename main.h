f3b76c4e6c9c99e295316a2916f6327d0f0e2030
Xenon22pc <59983834+Xenon22pc@users.noreply.github.com>
2020-04-01 10:38:11 +0300

    Create main.h

---
+++ main.h
@@ -0,0 +1,46 @@
+/* USER CODE BEGIN EFP */
+extern uint8_t JOY_DIR,
+		prev_dir;
+
+extern int16_t point;
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LCD_nCS_Pin GPIO_PIN_13
+#define LCD_nCS_GPIO_Port GPIOC
+#define LCD_RS_Pin GPIO_PIN_14
+#define LCD_RS_GPIO_Port GPIOC
+#define LCD_nWR_Pin GPIO_PIN_15
+#define LCD_nWR_GPIO_Port GPIOC
+#define BUZZER_Pin GPIO_PIN_7
+#define BUZZER_GPIO_Port GPIOA
+#define DB0_Pin GPIO_PIN_0
+#define DB0_GPIO_Port GPIOB
+#define DB1_Pin GPIO_PIN_1
+#define DB1_GPIO_Port GPIOB
+#define DB2_Pin GPIO_PIN_2
+#define DB2_GPIO_Port GPIOB
+#define LCD_nRD_Pin GPIO_PIN_10
+#define LCD_nRD_GPIO_Port GPIOB
+#define LCD_nRESET_Pin GPIO_PIN_11
+#define LCD_nRESET_GPIO_Port GPIOB
+#define BTN4_Pin GPIO_PIN_12
+#define BTN4_GPIO_Port GPIOB
+#define BTN3_Pin GPIO_PIN_13
+#define BTN3_GPIO_Port GPIOB
+#define BTN2_Pin GPIO_PIN_14
+#define BTN2_GPIO_Port GPIOB
+#define BTN1_Pin GPIO_PIN_15
+#define BTN1_GPIO_Port GPIOB
+#define LED_Pin GPIO_PIN_15
+#define LED_GPIO_Port GPIOA
+#define DB3_Pin GPIO_PIN_3
+#define DB3_GPIO_Port GPIOB
+#define DB4_Pin GPIO_PIN_4
+#define DB4_GPIO_Port GPIOB
+#define DB5_Pin GPIO_PIN_5
+#define DB5_GPIO_Port GPIOB
+#define DB6_Pin GPIO_PIN_6
+#define DB6_GPIO_Port GPIOB
+#define DB7_Pin GPIO_PIN_7
+#define DB7_GPIO_Port GPIOB
